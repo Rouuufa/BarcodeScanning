f5638d023e62bc7ca2e9595d9d790166507a7754
Abderraouf Kraiem <118401832+Rouuufa@users.noreply.github.com>
2024-07-01 13:46:29 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BarcodeScanning',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=open('requirement.txt').read().strip().split('\n'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
